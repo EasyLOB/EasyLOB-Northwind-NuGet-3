4ae4a02e2f51e67c641042fbcd1fc3321bebdf5a
Siegmar Gieseler <easylob@gmail.com>
2020-11-07 12:52:27 -0300

    -

--- Northwind.Mvc/Models/Activity-Profile/ActivityRoleProfile.cs
+++ Northwind.Mvc/Models/Activity-Profile/ActivityRoleProfile.cs
@@ -9,7 +9,7 @@ namespace EasyLOB.Activity.Data
         public static void OnSetupProfile(IZProfile profile)
         {
             profile.SetProfileProperty("RoleName", isGridVisible: true);
-            profile.SetProfileProperty("Operations", isGridVisible: true);
+            profile.SetProfileProperty("Operations", isGridVisible: true, isEditVisible: false);
 
             profile.SetProfileProperty("ActivityLookupText", isGridVisible: true);
         }
